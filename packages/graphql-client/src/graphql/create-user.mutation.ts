13f3be0b219df296ffafdc080ca63915bcad0b1f
Kun Ran <rankun203@gmail.com>
2018-06-27 11:25:03 +0800

    fix: createUser is now working

--- packages/graphql-client/src/graphql/create-user.mutation.ts
+++ packages/graphql-client/src/graphql/create-user.mutation.ts
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
 export const createUserMutation = gql`
-  mutation($user: CreateUserInput!) {
-    register(user: $user): ID
+  mutation register($user: CreateUserInput!) {
+    register(user: $user)
   }
 `;
